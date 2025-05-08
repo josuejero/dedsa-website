a1136b5275804667a5b794078898a2ead282d3fd
josue <josuejero@hotmail.com>
2025-05-08 08:35:57 -0400

    update

--- comment-remover-backup-20250508083315/.lintstagedrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix'],
-  '*.{css,md,json}': ['prettier --write'],
-};
--- comment-remover-backup-20250508083315/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center py-20">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-dsa-red"></div>
-    </div>
-  );
-}
