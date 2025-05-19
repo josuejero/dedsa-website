b177a4cf89cdc5b065527577b24f052390f000fa
josue <josuejero@hotmail.com>
2025-05-19 16:55:25 -0400

    update

--- frontend-backup/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export { useForm } from './useForm';
-export { useInfiniteScroll } from './useInfiniteScroll';
-export { useKeyboardShortcut } from './useKeyboardShortcut';
-export { useLocalStorage } from './useLocalStorage';
-export { useNewsletterSubscription } from './useNewsletterSubscription';
-export { useWindowSize } from './useWindowSize';
